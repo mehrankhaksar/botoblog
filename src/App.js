203dda7b5c313e28fd17edc2cd7c191878ef4222
Titty <tittystrength@gmail.com>
2023-09-01 16:44:32 +0330

    final code

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 import Layout from "./components/layout";
+import ScrollToTop from "./components/elements/ScrollToTop";
 import HomePage from "./components/templates/HomePage";
 import AuthorDetailsPage from "./components/templates/AuthorDetailsPage";
 import BlogDetailsPage from "./components/templates/BlogDetailsPage";
@@ -11,6 +12,7 @@ import BlogDetailsPage from "./components/templates/BlogDetailsPage";
 function App() {
   return (
     <Layout>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/authors/:slug" element={<AuthorDetailsPage />} />
